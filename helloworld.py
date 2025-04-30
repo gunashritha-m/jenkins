f8f0bc12f83b50f7db6bca809489b2ac731b7906
gunashritha-m <164651823+gunashritha-m@users.noreply.github.com>
2025-04-30 12:27:22 +0530

    Update helloworld.py

--- helloworld.py
+++ helloworld.py
@@ -4,6 +4,8 @@ print("Hello world")
 print("Update: This update will reflect once Jenkins auto builds after 5 minutes.")
 
 print("Update 2: Testing once more, just to be sure.")
+print("Polling test at Jenkins JOB C")
+
 
 print("Update 3: Woah, we really are doing this!")
 
